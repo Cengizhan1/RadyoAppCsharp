cbb3daab613acc394c225c4e2c9afd0ad54b12d5
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2021-06-06 12:54:34 +0300

    finish

---
+++ Radyo programı/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Radyo_programı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://powerpop.listenpowerapp.com/powerpop/mpeg/icecast.audio";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://powerturkeniyiler.listenpowerapp.com/powerturkeniyiler/mpeg/icecast.audio ";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://37.247.101.101:5934/; ";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://kralpopwmp.radyotvonline.com:80/; ";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
